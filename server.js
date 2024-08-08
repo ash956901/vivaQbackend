f338eafb734eaad57de6d91a9af740fc21073b41
Ashutosh Kumar <123724919+ash956901@users.noreply.github.com>
2024-08-08 18:46:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // MongoDB Atlas connection
-const mongoUri = 'REMOVED';
+const mongoUri =process.env.MONGODB_URI;
 mongoose.connect(mongoUri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
